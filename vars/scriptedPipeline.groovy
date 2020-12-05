bd8b14fcdd1976e53b9e9f94db2c41f503b44cbd
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2020-12-05 11:07:31 +0530

    Update scriptedPipeline.groovy

--- vars/scriptedPipeline.groovy
+++ vars/scriptedPipeline.groovy
@@ -1,1 +1,22 @@
-test
+def call(Map pipelineparam)
+{
+pipeline
+{
+node
+{
+  stage("checkout scm")
+  {
+    sh '''
+      git clone $REPO_NAME
+      git checkout $BRANCH
+      '''
+  }
+  stage("build")
+  {
+  sh '''
+    mvn clean install
+  '''
+  }
+}
+}
+}
